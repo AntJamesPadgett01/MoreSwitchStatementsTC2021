7d615110a0cd85af4316e36498745e3c945496b8
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-11 11:53:34 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MoreSwitchStatementsTC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is pretty hard to master.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is also tough to grasp.");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a pretty easy subject to master.");
+                    break;
+                default:
+                    Console.WriteLine($"Wow i haven't taken that subject. {subject} sounds interesting.");
+                    break;
+            }
+
+
+
+        }
+    }    
+        
+    
+}
